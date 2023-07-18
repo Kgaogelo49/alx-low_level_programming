a89e27cc0a081f3f74543e825ccb8f831959edfb
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-18 11:18:29 -0700

    asdfghjk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 		if (i < 9)
 		{
 		putchar(',');
-		}
-		if (i < 10)
-		{
 		putchar(' ');
 		}
 	}
