cb7e0b6d46a0a59bae3e4cf049e7d597b7f2b77a
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-18 05:48:28 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'z'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
