5c969fe20c45884b9f8936021d1c2efc8deef00f
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-22 12:53:34 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing lins diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+vid print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < n; n++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
