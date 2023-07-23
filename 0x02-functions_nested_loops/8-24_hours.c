25884e77afe7a72e7d310b633a32f805dd6455fa
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-23 13:09:48 -0700

    fghj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,27 +8,19 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int minute;
+	int hour;
 
-	for (a = 0; a <= 2; a++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			if ((a < 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+		_putchar('0' + hour / 10);
+		_putchar('0' + hour % 10);
+		_putchar(':');
+		_putchar('0' + minute / 10);
+		_putchar('0' + minute % 10);
+		_putchar('\n');
 		}
 	}
 }
