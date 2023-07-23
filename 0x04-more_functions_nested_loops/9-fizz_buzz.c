2f3ace638bb3081d3b70e15f7fd8d34d70f43c5a
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-23 14:34:25 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 
+/**
+ * main - checks for numbers divisible by 3 and  5 and prints fizzbuzz
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-	if (i % 3 == 0 && i % 5 == 0) 
+
+	if (i % 3 == 0 && i % 5 == 0)
 	{
 		printf("FizzBuzz ");
-	} 
+	}
 	else if (i % 3 == 0)
 	{
 		printf("Fizz ");
-	} 
-	else if (i % 5 == 0) 
+	}
+	else if (i % 5 == 0)
 	{
 		printf("Buzz ");
 	}
-	else 
+	else
 	{
 		printf("%d ", i);
 	}
 	}
 
 	printf("\n");
-	return 0;
+	return (0);
 }
