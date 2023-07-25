e700d5491982930310056b5ccb46a5395c4aa72f
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-25 08:55:58 -0700

    ./0-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * reset_to_98 - takes in pointer and updates it to 98
+ *@n: The interger to be updated
+ *
+ * Return : void
+ */
 void reset_to_98(int *n)
 {
 	if (n != NULL)
