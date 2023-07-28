2bd1f40657b51ad5de767f2a661feb07730130e3
Kgaogelo49 <1843244@students.wits.ac.za>
2023-07-28 03:47:28 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings overwriting the null byte
+ * @dest: string to be concatenated
+ * @src: string to be concatenated
+ *
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2, i;
+
+	for (len1 = 0; dest[len1] != '\0'; len1++)
+	{
+	}
+	for (len2 = 0; src[len2] != '\0'; len2++)
+	{
+	}
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + 1] = src[i];
+	}
+	return (dest);
+}
