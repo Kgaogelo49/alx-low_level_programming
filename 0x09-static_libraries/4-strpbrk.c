903b5126d371559db63c87ea4d900611b8832732
Kgaogelo Phooko <1843244@students.wits.ac.za>
2023-08-26 12:38:10 -0700

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -10,4 +10,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int t;
+
+	while (*s)
+	{
+		for (t = 0; accept[t]; t++)
+		{
+		if (*s == accept[t])
+		return (s);
+		}
+	s++;
+	}
+
+	return ('\0');
 }
