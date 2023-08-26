26528dfc5b262e5172f0756f2fdf8d82f91f3ab2
Kgaogelo Phooko <1843244@students.wits.ac.za>
2023-08-26 07:36:16 -0700

    Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,45 @@
 #include "main.h"
 
 /**
+ * _sqrt_assistant - performs a binary search to find the square root
+ * @upper: the upper bound of the search range
+ * @lower: lower bound of the search range
+ * @n: the number for which we want to find the square root
+ *
+ * Return: -1 if no natural square root found, or the square root number
+ */
+
+int _sqrt_assistant(int n, int lower, int upper)
+{
+	int mid;
+	int square;
+
+	if (lower > upper)
+	{
+		return (-1);
+	}
+
+	mid = (lower + upper) / 2;
+	square = (mid * mid);
+
+	if (square == n)
+	{
+		return (mid);
+	}
+	else if (square < n)
+	{
+		return (_sqrt_assistant(n, mid + 1, upper));
+	}
+	else
+	{
+		return (_sqrt_assistant(n, lower, mid - 1));
+	}
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to be square rooted
  *
- * Return: 
+ * Return: -1 if n is negative, 0r the square root of n
  */
 
 int _sqrt_recursion(int n)
@@ -13,3 +48,5 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
+		return (_sqrt_assistant(n, 0, n));
+}
