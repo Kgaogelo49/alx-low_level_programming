eb05e3240bcd3055cf5c46f964f688b8ffb39bce
Kgaogelo Phooko <1843244@students.wits.ac.za>
2023-08-26 07:39:39 -0700

    Write a function that returns the natural square root of a number.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int is_prime_helper(int n, int divisor);
 int is_palindrome_helper(char *s, int start, int end);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _sqrt_assistant(int n, int lower, int upper);
 
 #endif /* MAIN_H */
